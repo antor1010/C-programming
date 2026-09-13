1f0715affc01ca666e979716add57ee793ce46de
Antor Biswas <151869057+antor1010@users.noreply.github.com>
2026-09-13 10:03:04 +0530

    Add files via upload

---
+++ Basic/basicdatatype.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int number = 42;        
+    float price = 10.5f;    
+    double pi = 3.14159;    
+    char grade = 'A';       
+    printf("Int %d\n",number);
+    printf("flaot %f\n",price);
+    printf("Double %lf\n",pi);
+    printf("Char %c\n",grade);
+    return 0;
+}
